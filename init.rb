6e968da199e79e4d3620ac3435eabca616ec86d8
berkekalkan <berkekalkantr@gmail.com>
2021-06-08 11:35:06 -0700

    Generate redmine plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :issue_lifecycle_info do
+  name 'Issue Lifecycle Info plugin'
+  author 'Berke Kalkan'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'https://github.com/berkeka/redmine_issue_lifecyle_info'
+  author_url 'https://github.com/berkeka'
+end
