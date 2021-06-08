5bd24c0aa26bd2ba3bbf09a2022120f1b41e8d22
berkekalkan <berkekalkantr@gmail.com>
2021-06-08 14:28:57 -0700

    Add menu item and permission for lifecycle info

--- init.rb
+++ init.rb
@@ -5,4 +5,8 @@ Redmine::Plugin.register :issue_lifecycle_info do
   version '0.0.1'
   url 'https://github.com/berkeka/redmine_issue_lifecyle_info'
   author_url 'https://github.com/berkeka'
+
+  permission :view_lifecycle_info, issue_lifecycle_info: [:index]
+
+  menu :project_menu, :issue_lifecycle_info, { controller: 'issue_lifecycle_info', action: 'index' }, caption: 'Issue lifecycle info', after: :activity, param: :project_id
 end
